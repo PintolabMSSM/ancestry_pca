44a7a53d2df96a1057b89fb3e74442cd3da7d6eb
Clara Pereira <clarafap@gmail.com>
2021-06-24 21:36:22 +0100

    upgraded plink version; updated links to minerva

--- preprocess/vcf_to_plink.sh
+++ preprocess/vcf_to_plink.sh
@@ -2,11 +2,13 @@
 
 # Convert a .vcf file to a plink binary fileset (.bed, .bim, .fam)
 
-module load plink/1.90
+#module load plink/1.90
+ml plink2/2.3
 
-vcfdir=/sc/arion/scratch/belmoj01/splicingQTL/
+vcfdir=/sc/arion/projects/EPIASD/splicingQTL/output/vcf
 vcffile=Capstone4.sel.hasPhenosOnly.idsync.vcf
+vcfoutdir=/sc/arion/projects/EPIASD/splicingQTL/output/vcf/plink2_v2.3
 
 mkdir -p $vcfoutdir
 
-plink --vcf $vcfdir/$vcffile --double-id --make-bed --out $vcfdir/$vcffile
+plink2 --vcf $vcfdir/$vcffile --double-id --make-bed --out $vcfoutdir/$vcffile
