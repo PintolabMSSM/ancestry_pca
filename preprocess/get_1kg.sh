9e832ef7e40f5701683c1f83b708dc97452046e5
Clara Pereira <clarafap@gmail.com>
2021-06-24 21:15:01 +0100

    addresses and paths corrected

--- preprocess/get_1kg.sh
+++ preprocess/get_1kg.sh
@@ -4,23 +4,23 @@
 
 # Get 1kg phase 3 from remote source & convert to plink binary format 
 
-refdir=/sc/arion/projects/EPIASD/splicingQTL/PCA/1kg_phase3
+refdir=/sc/arion/projects/EPIASD/splicingQTL/scripts/pre_fastQTL/PCA/1kg_phase3
 mkdir -p $refdir/plink_log
 
 cd $refdir
 
 # If DropBox links are dead, get from https://www.cog-genomics.org/plink/2.0/resources#1kg_phase3
 pgen=https://www.dropbox.com/s/afvvf1e15gqzsqo/all_phase3.pgen.zst?dl=1
-pvar=https://www.dropbox.com/s/0nz9ey756xfocjm/all_phase3.pvar.zst?dl=1
+pvar=https://www.dropbox.com/s/op9osq6luy3pjg8/all_phase3.pvar.zst?dl=1 # Note: address in link didn't work, has been replaced
 sample=https://www.dropbox.com/s/yozrzsdrwqej63q/phase3_corrected.psam?dl=1
 
+ml plink2/2.3
 
 # Download and decompress 1000 Genomes phase 3 data
 wget $pgen
 mv 'all_phase3.pgen.zst?dl=1' all_phase3.pgen.zst
 plink2 --zst-decompress all_phase3.pgen.zst > all_phase3.pgen
 
-# Note: address doesn't work, get from here instead: https://www.dropbox.com/s/op9osq6luy3pjg8/all_phase3.pvar.zst?dl=1
 wget $pvar
 mv 'all_phase3.pvar.zst?dl=1' all_phase3.pvar.zst
 
@@ -28,10 +28,9 @@ wget $sample
 mv 'phase3_corrected.psam?dl=1' all_phase3.psam
 
 # Convert 1000 Genomes phase 3 data to plink 1 binary format
-ml plink2
 
 plink2 --pfile $refdir/all_phase3 vzs\
       --max-alleles 2 \
       --make-bed \
       --out $refdir/all_phase3
-mv $refdir/all_phase3.log $refdir/log
+mv $refdir/all_phase3.log $refdir/plink_log
