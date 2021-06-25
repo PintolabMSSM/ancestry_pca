d2f8fac256f78ac0ad8b704f4583db410360b0c2
Clara Pereira <clarafap@gmail.com>
2021-06-25 03:55:45 +0100

    back to plinkv1.9

--- preprocess/vcf_to_plink.sh
+++ preprocess/vcf_to_plink.sh
@@ -2,13 +2,13 @@
 
 # Convert a .vcf file to a plink binary fileset (.bed, .bim, .fam)
 
-#module load plink/1.90
-ml plink2/2.3
+module load plink/1.90
+#ml plink2/2.3
 
 vcfdir=/sc/arion/projects/EPIASD/splicingQTL/output/vcf
 vcffile=Capstone4.sel.hasPhenosOnly.idsync.vcf
-vcfoutdir=/sc/arion/projects/EPIASD/splicingQTL/output/vcf/plink2_v2.3
+vcfoutdir=/sc/arion/projects/EPIASD/splicingQTL/output/vcf/plink1.9
 
 mkdir -p $vcfoutdir
 
-plink2 --vcf $vcfdir/$vcffile --double-id --make-bed --out $vcfoutdir/$vcffile
+plink --vcf $vcfdir/$vcffile --double-id --make-bed --out $vcfoutdir/$vcffile
